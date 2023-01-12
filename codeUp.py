ee040fa78ecd6339f39e8196b0ab8956d4dc3927
Bong <2021bongwonny@gmail.com>
2023-01-12 23:25:00 +0900

    ✨ CodeUp Python 기초 100제     - 6개 풀이     - 6개 풀이

--- codeUp.py
+++ codeUp.py
@@ -54,3 +54,29 @@ print(bool(m))
 a = True
 b = False
 print((a and (not b)) or ((not a) and b))
+
+# \** 비트단위(bitwise) 연산자
+# ~(bitwise not) : (~ : tilde, 틸드라고 읽는다.)
+
+# &(bitwise and) : (and, ampersand, 앰퍼센드라고 읽는다.) 
+
+# |(bitwise or) : (or, vertical bar, 버티컬바, 파이프(pipe)연산자라고도 불리는 경우가 있다.)
+
+# ^(bitwise xor) : (xor, circumflex/caret, 서컴플렉스/카릿), (C언어에서는 전혀 다른 배타적 논리합(xor, 서로 다를 때 1)의 의미를 가진다.)
+# ^를 구체적으로 설명하자면, 두 장의 이미지가 겹쳐졌을 때 색이 서로 다른 부분만 처리할 수 있다.
+# 배경이 되는 그림과 배경 위에서 움직이는 그림이 있을 때,
+# 두 그림에서 차이만 골라내 배경 위에서 움직이는 그림의 색으로 바꿔주면 전체 그림을 구성하는 모든 점들의 색을 다시 계산해 입히지 않고 보다 효과적으로 그림을 처리할 수 있게 되는 것이다.
+# 비행기 슈팅게임 등을 상상해보면 된다.
+
+# <<(bitwise left shift), >>(bitwise right shift)
+
+# 3항연산
+# 3개의 요소로 이루어지는 3항 연산은
+# "x if C else y" 의 형태로 작성이 된다.
+# - C : True 또는 False 를 평가할 조건식(conditional expression) 또는 값
+# - x : C의 평가 결과가 True 일 때 사용할 값
+# - y : C의 평가 결과가 True 가 아닐 때 사용할 값
+# 중첩도 가능하며 연산자 우선순위에 따라 실행되니 주의한다. 
+# 예 : (a if a>b else b) if ((a if a>b else b)>c) else c
+n1, n2, n3 = 3, -1, 5
+print((n1 if n1 < n2 else n2) if ((n1 if n1 < n2 else n2) < n3) else n3)
