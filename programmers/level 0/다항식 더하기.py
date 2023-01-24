165be17b06402921d33062c95208ead400710abd
Bong <2021bongwonny@gmail.com>
2023-01-24 19:35:55 +0900

    🩹 프로그래머스 level 0 다항식 더하기 (다시 풀기) - python 풀이 (검색 후 틀린 이유 찾아서 통과)

--- programmers/level 0/다항식 더하기.py
+++ programmers/level 0/다항식 더하기.py
@@ -1,36 +1,66 @@
 # 다항식 더하기
 
-def solution(polynomial):
-    if len(polynomial) == 1 or len(polynomial) == 2:
-        return polynomial
-    xNum = []
-    num =  []
-    for v in polynomial.split(' + '):
-      if 'x' in v:
-        if v == 'x':
-          xNum.append(1)
-        else:
-          xNum.append(int(v.split('x')[0]))
-      else:
-        num.append(int(v))
+# 첫번째 풀이 (5, 8, 12 통과 못함)
+# def solution(polynomial):
+#     if len(polynomial) == 1 or len(polynomial) == 2:
+#         return polynomial
+#     xNum = []
+#     num =  []
+#     for v in polynomial.split(' + '):
+#       if 'x' in v:
+#         if v == 'x':
+#           xNum.append(1)
+#         else:
+#           xNum.append(int(v.split('x')[0]))
+#       else:
+#         num.append(int(v))
   
-    if len(xNum) != 0 and len(num) != 0:
-      xNum = sum(xNum)
-      num = sum(num)
-    elif len(xNum) == 0:
-      return sum(num)
-    elif len(num) == 0:
-      return str(sum(xNum))+'x'
+#     if len(xNum) != 0 and len(num) != 0:
+#       xNum = sum(xNum)
+#       num = sum(num)
+#     elif len(xNum) == 0:
+#       return sum(num)
+#     elif len(num) == 0:
+#       return str(sum(xNum))+'x'
       
-    ans = [str(xNum), 'x', ' + ', str(num)]
-    return ''.join(ans)
-    
+#     ans = [str(xNum), 'x', ' + ', str(num)]
+#     return ''.join(ans)
 
-print(solution("3x + 7 + x"))
-print(solution("x + x + x"))
+
+# 검색 후 두번째 풀이 ( 1x를 처리를 안해줘서 그런게 아닐까? )
+def solution(polynomial):
+  if len(polynomial) == 1 or len(polynomial) == 2:
+      return polynomial
+  xNum = 0
+  num =  0
+  for v in polynomial.split(' + '):
+    if 'x' in v:
+      if v == 'x':
+        xNum += 1
+      else:
+        xNum += int(v.split('x')[0])
+    else:
+      num += int(v)
+  if xNum == 1:
+    if num > 0:
+      return 'x'+' + '+str(num)
+    else:
+      return 'x'
+  elif xNum != 1 and xNum > 0:
+    if num > 0:
+      return str(xNum)+'x'+' + '+str(num)
+    else:
+      return str(xNum)+'x'
+  else:
+    return str(num)
+  # 마지막에 숫자가 아니라 문자로 리턴해줘야 5번 케이스 통과
+
+# print(solution("3x + 7 + x"))
+# print(solution("x + x + x"))
+
+
 
 # JS풀이 (5, 8, 12 통과 못함)
-
 # function solution(polynomial) {
 #     if (polynomial.length === 1 || polynomial.length === 2) return polynomial;
 #     const splitStr = polynomial.split(' + ')
